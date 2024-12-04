2b02b8b77d945c47bbd15d0471229f76d6e43d14
Kona <conradulysses@outlook.com>
2024-12-04 08:41:46 -0800

    Starting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
+import java.io.File
+
+//val input = File("input.txt")
+val input = """MMMSXXMASM
+    MSAMXMSMSA
+    AMXSXMAAMM
+    MSAMASMSMX
+    XMASAMXAMM
+    XXAMMXXAMA
+    SMSMSASXSS
+    SAXAMASAAA
+    MAMMMXMMMM
+    MXMXAXMASX""".trimIndent()
+
+val dataX = input.split("\n")
+val dataY = mutableListOf<String>()
+var xIncrement = 0
+
 fun main () {
-    println("Hello World!")
+    for(elements in dataX){
+        dataY.add(dataX[xIncrement])
+        xIncrement += 1
+    }
+    println(dataX[0])
 }
